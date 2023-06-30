81dfa8a64c3941be73f31059fecaa7b7d61a801c
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2023-06-30 23:10:00 +0700

    Add title Tab

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang='en'>
       <Head>
-        <title>My Link | Mahdy Mubasyir</title>
         <meta name='description' content='Mahdy Mubasyir Link' />
         <meta property='og:title' content='Link Management' />
         <meta property='og:description' content='Mahdy Mubasyir Portfolio' />
