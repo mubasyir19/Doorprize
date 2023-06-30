e8a55596d0cdf40dc2abe57cd263a517f22a0b85
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2023-06-30 17:18:57 +0700

    Create Layout Component Button

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,5 +1,29 @@
 import React from 'react';
+import Image from 'next/image';
+import { Icon } from '@iconify/react';
 
-export default function Button() {
-  return <div>Button</div>;
+export function ButtonImage({ link, image, text, bgColor }) {
+  return (
+    <div className='my-4'>
+      <a href={link} className={`${bgColor} w-full py-3 rounded-md flex justify-center gap-x-2`} target='_blank'>
+        <Image src={image} height={32} width={32} className='rounded-md' alt='logo' />
+        <span className='my-auto'>{text}</span>
+      </a>
+    </div>
+  );
+}
+
+export function ButtonIcon({ link, icon, text, bgColor, border }) {
+  return (
+    <div className='my-4'>
+      <a
+        href={link}
+        className={`${bgColor} w-full py-3 rounded-md flex justify-center gap-x-2 ${border}`}
+        target='_blank'
+      >
+        <Icon icon={`skill-icons:${icon}`} className='text-2xl' />
+        {text}
+      </a>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Image from 'next/image';
-import { Inter } from 'next/font/google';
-import { Icon } from '@iconify/react';
+import { ButtonIcon, ButtonImage } from '@/components/Button';
 
 export default function Home() {
   return (
@@ -18,46 +17,31 @@ export default function Home() {
         </i>
       </div>
       <div className='mt-12 text-white'>
-        <div className='my-4'>
-          <a
-            href='https://mahdy-mubasyir.vercel.app/'
-            className='bg-blue-900 w-full py-3 rounded-md flex justify-center gap-x-2'
-            target='_blank'
-          >
-            <Image src='/logo.png' height={32} width={32} className='rounded-md' alt='logo' />
-            <span className='my-auto'>Portfolio </span>
-          </a>
-        </div>
-        <div className='my-4'>
-          <a
-            href='https://www.linkedin.com/in/mahdy-mubasyir-699424210'
-            className='bg-blue-600 w-full py-3 rounded-md flex justify-center gap-x-2'
-            target='_blank'
-          >
-            <Icon icon='skill-icons:linkedin' className='text-2xl' />
-            LinkedIn
-          </a>
-        </div>
-        <div className='my-4'>
-          <a
-            href='https://www.instagram.com/mubasyirmahdy'
-            className='bg-gradient-to-r from-yellow-500 via-red-500 to-purple-700 w-full py-3 rounded-md flex justify-center gap-x-2'
-            target='_blank'
-          >
-            <Icon icon='skill-icons:instagram' className='text-2xl' />
-            Instagram
-          </a>
-        </div>
-        <div className='my-4'>
-          <a
-            href='https://github.com/mubasyir19'
-            className='bg-black w-full py-3 rounded-md flex justify-center gap-x-2 border-2 border-white'
-            target='_blank'
-          >
-            <Icon icon='skill-icons:github-dark' className='text-2xl' />
-            Github
-          </a>
-        </div>
+        <ButtonImage
+          link={'https://github.com/mubasyir19'}
+          image={'/logo.png'}
+          text={'Portfolio'}
+          bgColor={'bg-blue-900'}
+        />
+        <ButtonIcon
+          link={'https://www.linkedin.com/in/mahdy-mubasyir-699424210'}
+          icon={'linkedin'}
+          text={'LinkedIn'}
+          bgColor={'bg-blue-600'}
+        />
+        <ButtonIcon
+          link={'https://www.instagram.com/mubasyirmahdy'}
+          icon={'instagram'}
+          text={'Instagram'}
+          bgColor={'bg-gradient-to-r from-yellow-500 via-red-500 to-purple-700'}
+        />
+        <ButtonIcon
+          link={'https://github.com/mubasyir19'}
+          icon={'github-dark'}
+          text={'Github'}
+          bgColor={'bg-black'}
+          border={'border-2 border-white'}
+        />
       </div>
       <footer className='mt-8 text-center text-white'>
         <p>Made with &#x2764;&#xfe0f; by Maher</p>
